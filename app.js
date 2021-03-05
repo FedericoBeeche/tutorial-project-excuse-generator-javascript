e085b54aa0b82cdab188a6c720ae7120b6a96a47
FedericoBeeche <fbeeche@gmail.com>
2021-03-05 17:13:29 +0000

    inicio del proyecto

---
+++ app.js
@@ -0,0 +1,7 @@
+function onLoad(){
+    let who = ['My dad', 'My wife', 'My dog', 'His cat', 'Her pet lizard'];
+    let action = ['ate', 'pooped', 'peed', 'threw', 'hid', 'burned'];
+    let what = ['my homework', 'my phone', 'my computer', 'my tablet', 'the money'];
+    let when = ['right before the class', 'while I was sleeping', 'during my lunch', 'when I was taking a shower']
+
+}
